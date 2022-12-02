3254aee39f501169924f12da6b6198bf85e11e63
Daniel Civit <daniel.civit@multisafepay.com>
2022-12-02 13:58:53 +0100

    PLGMAG2V2-581: Fix release action (#77)

--- .github/workflows/magento-integration-tests-pre-phpunit.sh
+++ .github/workflows/magento-integration-tests-pre-phpunit.sh
@@ -6,22 +6,62 @@ if [[ $GITHUB_REPOSITORY == *"internal"* ]] ; then
     REPO_SUFFIX="-internal"
 fi
 
-REPOSITORIES=$(cat <<HERE
-php-sdk${REPO_SUFFIX}
-magento2${REPO_SUFFIX}
-magento2${REPO_SUFFIX}-core
-magento2${REPO_SUFFIX}-frontend
-magento2${REPO_SUFFIX}-adminhtml
-magento2${REPO_SUFFIX}-catalog-inventory
-magento2${REPO_SUFFIX}-msi
-HERE
-)
+# PHP-SDK
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    PHP_SDK_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    PHP_SDK_BRANCH_NAME="master"
+fi
+git clone -b ${PHP_SDK_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/php-sdk${REPO_SUFFIX}.git ./package-source/multisafepay/php-sdk/
 
-for REPOSITORY in $REPOSITORIES; do
-    composer config repositories.multisafepay-$REPOSITORY vcs git@github.com:MultiSafepay/$REPOSITORY.git
-done
+# MAGENTO 2 META PACKAGE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    CORE_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    CORE_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${CORE_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}.git ./package-source/multisafepay/magento2/
 
+# MAGENTO 2 CORE MODULE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-core/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    CORE_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    CORE_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${CORE_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-core.git ./package-source/multisafepay/magento2-core/
+
+# MAGENTO 2 ADMIN MODULE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    ADMIN_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml.git ./package-source/multisafepay/magento2-adminhtml/
+
+# MAGENTO 2 FRONTEND
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-frontend/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    ADMIN_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-frontend.git ./package-source/multisafepay/magento2-frontend/
+
+# MAGENTO 2 CATALOG INVENTORY
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-catalog-inventory/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    ADMIN_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-catalog-inventory.git ./package-source/multisafepay/magento2-catalog-inventory/
+
+# MAGENTO 2 MSI
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-msi/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    ADMIN_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-msi.git ./package-source/multisafepay/magento2-msi/
+
+composer config repositories.multisafepay "path" "package-source/multisafepay/*"
 composer config minimum-stability dev
 composer config prefer-stable false
-
-composer require yireo/magento2-replace-bundled:^4.1 --no-update
